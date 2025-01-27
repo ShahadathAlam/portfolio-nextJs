e4025435831ad55c957443ac06682f3f35f4c8f5
ShahadathAlam <shahadathalam@ymail.com>
2025-01-27 21:43:44 +0600

    some fixes are sovled in BentoGrid component

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
 
-        {/* <Grid /> */}
+        <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
