481fc63d173f34824f0d7ed64c5a1c6d2b6f2334
ShahadathAlam <shahadathalam@ymail.com>
2025-01-27 19:34:09 +0600

    sentry removed

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    // Optional: You can log errors here without Sentry if needed.
+    console.error(error);
   }, [error]);
 
   return (
